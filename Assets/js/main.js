9e6773a887fad9a953ade45bacffcfcb18058492
N3rdy <n3rdydzn@hotmail.com>
2023-02-02 12:53:37 -0300

    :anchor: - ablalblafkafrijg09325r80921390rjf90sdafh98nsa

---
+++ Assets/js/main.js
@@ -0,0 +1,10 @@
+
+var texto = document.getElementById('m_title');
+
+var typewriter = new Typewriter(texto, {
+    strings: ['N3RDY', 'N3RDYDZN', 'C:\\N3RDY'],
+    autoStart: true,
+    loop: true,
+    pauseFor: 2000,
+    devMode: true,
+});
